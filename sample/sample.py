15f38637886a4cad3cf29d6b3c517dfab9230582
KugaKiri <ancient.knight.0f.round@gmail.com>
2026-01-28 11:40:49 +0900

    260128サンプルコードの更新

--- sample/sample.py
+++ sample/sample.py
@@ -1,9 +1,25 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
-input_num = st.number_input('Input a number', value=0)
+st.title("Sample App")
 
-result = input_num ** 2
-st.write('Result: ', result)
-st.title("Sample calculator")
-st.divider()
-st.set_page_config(page_title="Sample Calculator")
+num = st.number_input("Input a number", value=0)
+result = num ** 2
+st.write("Result:", result)
+
+if st.button("画像として保存"):
+    fig, ax = plt.subplots()
+    ax.text(0.5, 0.6, f"Input: {num}", ha="center", fontsize=14)
+    ax.text(0.5, 0.4, f"Result: {result}", ha="center", fontsize=14)
+    ax.axis("off")
+
+    file_name = "result.png"
+    fig.savefig(file_name)
+
+    with open(file_name, "rb") as f:
+        st.download_button(
+            label="画像をダウンロード",
+            data=f,
+            file_name=file_name,
+            mime="image/png"
+        )
