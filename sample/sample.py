286dbe1c0b7bf7ef72c4c91657b1066ff16f346e
KugaKiri <ancient.knight.0f.round@gmail.com>
2026-01-28 11:14:51 +0900

    260128sampleのタイトルを追加

--- sample/sample.py
+++ sample/sample.py
@@ -4,3 +4,6 @@ input_num = st.number_input('Input a number', value=0)
 
 result = input_num ** 2
 st.write('Result: ', result)
+st.title("Sample calculator")
+st.divider()
+st.set_page_config(page_title="Sample Calculator")
